d4b2e567950919b33dd5f1ba8601647638b7c919
aam <aam@anahoret.com>
2017-09-26 15:11:13 +0300

    Fix namespace.

--- clojure/test/clojure_getting_started/web_test.clj
+++ clojure/test/clojure_getting_started/web_test.clj
@@ -1,6 +1,6 @@
 (ns clojure-getting-started.web-test
   (:require [clojure.test :refer :all]
-            [clojure-getting-started.web :refer :all]))
+            [com-anahoret-bot.web :refer :all]))
 
 (deftest first-test
   (is false "Tests should be written"))
