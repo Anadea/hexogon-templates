3878d2288848eb94040039486261a9d5e17e6b97
aam <aam@anahoret.com>
2017-09-26 15:22:17 +0300

    Update dependencies versions.

--- clojure/project.clj
+++ clojure/project.clj
@@ -1,14 +1,13 @@
 (defproject com-anahoret-bot "1.0.0-SNAPSHOT"
   :description "Demo Clojure Bot"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [ring/ring-core "1.5.0"]
-                 [ring/ring-jetty-adapter "1.5.0"]
+                 [ring/ring-core "1.6.2"]
+                 [ring/ring-jetty-adapter "1.6.2"]
                  [ring/ring-json "0.4.0"]
-                 [ring-middleware-format "0.7.0"]
-                 [org.clojure/tools.logging "0.3.1"]
-                 [compojure "1.5.1"]
-                 [org.clojure/tools.logging "0.3.1"]
-                 [environ "1.0.0"]]
+                 [ring-middleware-format "0.7.2"]
+                 [org.clojure/tools.logging "0.4.0"]
+                 [compojure/compojure "1.6.0"]
+                 [environ "1.1.0"]]
   :min-lein-version "2.0.0"
   :main com-anahoret-bot.web
   :plugins [[environ/environ.lein "0.3.1"]]
